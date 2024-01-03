add644cfbd1181aeadad239e24fc502e04866581
Juste120 <pakoujuste2019@gmail.com>
2024-01-03 20:58:32 +0000

    authentification avec token

---
+++ src/main/java/flutter/backend/projetflutter/ProjetFlutterApplication.java
@@ -0,0 +1,13 @@
+package flutter.backend.projetflutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFlutterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFlutterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flutter/backend/projetflutter/ProjetFlutterApplicationTests.java
@@ -0,0 +1,13 @@
+package flutter.backend.projetflutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFlutterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
